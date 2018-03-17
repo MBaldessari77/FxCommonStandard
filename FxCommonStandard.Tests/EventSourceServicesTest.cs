73ee07481db0002a853db0f091862c811a9c4f08
Marco Baldessari <mb@sinformatic.com>
2018-03-17 20:20:39 +0100

    Working on EventSourcing Services

---
+++ FxCommonStandard.Tests/EventSourceServicesTest.cs
@@ -0,0 +1,33 @@
+﻿using FxCommonStandard.Services;
+using Xunit;
+
+namespace FxCommonStandard.Tests
+{
+	public class EventSourceServicesTest
+	{
+		[Fact]
+		public void WhenAnEventIsRaisedTheListenerReceiveTheEvent()
+		{
+			int raisedCount=0;
+			var service = new EventSourceService();
+
+			service.RegisterListener((sender, args) => { raisedCount++; });
+			service.RegisterEvent();
+
+			Assert.Equal(1, raisedCount);
+		}
+
+		[Fact]
+		public void WhenAParticularEventIsRaisedTheListenerRegisterToThisEventReceiveTheEvent()
+		{
+			int raisedCount=0;
+			var service = new EventSourceService();
+
+			service.RegisterListener((sender, args) => { raisedCount++; }, "ParticularEvent");
+			service.RegisterListener((sender, args) => { raisedCount++; }, "AnotherEvent");
+			service.RegisterEvent("ParticularEvent");
+
+			Assert.Equal(1, raisedCount);
+		}
+	}
+}
---
+++ FxCommonStandard/Services/EventSourceServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FxCommonStandard.Services
+{
+	public class EventSourceService
+	{
+		event EventHandler<string> EventHappened;
+
+		public void RegisterListener(EventHandler<string> @delegate, string key = null) { EventHappened += (sender, e) => RaiseEvent(sender, e, key, @delegate); }
+		public void RegisterEvent(string key = null) { OnEventHappened(key); }
+
+		void OnEventHappened(string key) { EventHappened?.Invoke(this, key); }
+
+		void RaiseEvent(object sender, string key, string expectedKey, EventHandler<string> @delegate)
+		{
+			if (Equals(expectedKey, key))
+				@delegate(sender, key);
+		}
+	}
+}
