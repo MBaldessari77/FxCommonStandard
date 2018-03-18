0a54d28fd702cb6bf1447b938176fcab13a1f87e
Marco Baldessari <mb@sinformatic.com>
2018-03-18 14:30:28 +0100

    Working on EventSourcing Services

--- FxCommonStandard.Tests/EventSourceServicesTest.cs
+++ FxCommonStandard.Tests/EventSourceServicesTest.cs
@@ -1,22 +1,34 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 using FxCommonStandard.Services;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace FxCommonStandard.Tests
 {
 	public class EventSourceServicesTest
 	{
+		private readonly ITestOutputHelper _output;
+
+		public EventSourceServicesTest(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
 		[Fact]
 		public async void WhenAnEventIsRaisedTheListenerReceiveTheEvent()
 		{
 			int raisedCount = 0;
-			var service = new EventSourceService();
 
-			service.SubscribeEvent((sender, args) => { raisedCount++; });
+			using (var service = new EventSourceService())
+			{
+				service.SubscribeEvent((sender, args) => { raisedCount++; });
 
-			service.AddEvent();
-			await service.EventsProcessedAsync();
+				service.AddEvent();
+
+				await service.WaitEventsProcessedAsync();
+			}
 
 			Assert.Equal(1, raisedCount);
 		}
@@ -25,28 +37,56 @@ namespace FxCommonStandard.Tests
 		public async void WhenAParticularEventIsRaisedTheListenerRegisterToThisEventReceiveTheEvent()
 		{
 			int raisedCount = 0;
-			var service = new EventSourceService();
 
-			service.SubscribeEvent((sender, args) => { raisedCount++; }, new CustomEventArgs());
-			service.SubscribeEvent((sender, args) => { raisedCount++; });
-			
-			service.AddEvent(new CustomEventArgs());
-			await service.EventsProcessedAsync();
+			using (var service = new EventSourceService())
+			{
+				service.SubscribeEvent((sender, args) => { raisedCount++; }, new CustomEventArgs());
+				service.SubscribeEvent((sender, args) => { raisedCount++; });
+
+				service.AddEvent(new CustomEventArgs());
+
+				await service.WaitEventsProcessedAsync();
+			}
 
 			Assert.Equal(1, raisedCount);
 		}
 
 		[Fact]
-		public async void AStallOnListenerOnEventCantBlockOtherListener()
+		public async void WhenAnEventIsRaisedAllTheListenerReceiveTheEvent()
+		{
+			int raisedCount1 = 0;
+			int raisedCount2 = 0;
+
+			using (var service = new EventSourceService())
+			{
+				service.SubscribeEvent((sender, args) => { raisedCount1++; });
+				service.SubscribeEvent((sender, args) => { raisedCount2++; });
+
+				service.AddEvent();
+
+				await service.WaitEventsProcessedAsync();
+			}
+
+			Assert.Equal(1, raisedCount1);
+			Assert.Equal(1, raisedCount2);
+		}
+
+		[Fact]
+		public async void AStallOnAListenerOnEventCantBlockOtherListener()
 		{
 			int raisedCount = 0;
-			var service = new EventSourceService();
+			using (var service = new EventSourceService())
+			{
+				service.SubscribeEvent((sender, args) => { Thread.Sleep(int.MaxValue); });
+				service.SubscribeEvent((sender, args) => { raisedCount++; });
 
-			service.SubscribeEvent((sender, args) => { Thread.Sleep(int.MaxValue); });
-			service.SubscribeEvent((sender, args) => { raisedCount++; });
+				service.AddEvent();
 
-			service.AddEvent();
-			await service.EventsProcessedAsync(100);
+				Stopwatch stopwatch=new Stopwatch();
+				stopwatch.Start();
+				await service.WaitEventsProcessedAsync(100);
+				_output.WriteLine($"ElapsedMilliseconds = {stopwatch.ElapsedMilliseconds}");
+			}
 
 			Assert.Equal(1, raisedCount);
 		}
--- FxCommonStandard/Services/EventSourceServices.cs
+++ FxCommonStandard/Services/EventSourceServices.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Linq;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,55 +8,94 @@ namespace FxCommonStandard.Services
 {
 	public class EventSourceService : IDisposable
 	{
+		readonly ManualResetEventSlim _event = new ManualResetEventSlim(false);
 		readonly ConcurrentQueue<EventArgs> _eventQueue = new ConcurrentQueue<EventArgs>();
-		readonly ConcurrentQueue<EventArgs> _eventProcessingQueue = new ConcurrentQueue<EventArgs>();
 		readonly ConcurrentBag<Tuple<EventHandler, EventArgs>> _eventMapping = new ConcurrentBag<Tuple<EventHandler, EventArgs>>();
 
+		long _processingEvent;
 		bool _disposed;
 
 		public EventSourceService() { new Thread(EventSourcingWorker).Start(); }
-		public void Dispose() { _disposed = true; }
+
+		public void Dispose()
+		{
+			Signal();
+			_disposed = true;
+		}
 
 		public void SubscribeEvent(EventHandler @delegate, EventArgs expected = null) { _eventMapping.Add(new Tuple<EventHandler, EventArgs>(@delegate, expected)); }
 
-
 		public void AddEvent(EventArgs e = null)
 		{
+			//Update event per delegate count
 			foreach (Tuple<EventHandler, EventArgs> tuple in _eventMapping)
 				if (Equals(tuple.Item2, e))
-					_eventQueue.Enqueue(e);
+					Interlocked.Increment(ref _processingEvent);
+
+			_eventQueue.Enqueue(e);
+
+			Signal();
 		}
 
-		public Task EventsProcessedAsync(int timeoutMs = 0)
+		public Task WaitEventsProcessedAsync(int millisecondsTimeout = -1)
 		{
-			while (!_eventQueue.IsEmpty || !_eventProcessingQueue.IsEmpty)
+			return Task.Run(() =>
 			{
-				Thread.Sleep(timeoutMs);
-				if (timeoutMs > 0)
-					break;
-			}
-			return Task.CompletedTask;
+				Stopwatch stopwatch = new Stopwatch();
+				stopwatch.Start();
+				while (true)
+				{
+					if (Interlocked.Read(ref _processingEvent) == 0)
+						break;
+					WaitSignal(millisecondsTimeout);
+					if (millisecondsTimeout >= 0 && stopwatch.ElapsedMilliseconds > millisecondsTimeout)
+						break;
+				}
+			});
 		}
 
 		void EventSourcingWorker()
 		{
 			while (!_disposed)
 			{
-				if (_eventQueue.TryPeek(out var e) && !_eventProcessingQueue.Contains(e))
+				if (_eventQueue.TryDequeue(out var e))
+				{
+					ResetSignal();
+
 					foreach (Tuple<EventHandler, EventArgs> tuple in _eventMapping)
 						if (Equals(tuple.Item2, e))
-						{
-							_eventProcessingQueue.Enqueue(e);
 							Task.Run(() =>
 							{
-								tuple.Item1.Invoke(this, tuple.Item2);
-								_eventQueue.TryDequeue(out e);
-								_eventProcessingQueue.TryDequeue(out e);
-							});
-						}
+								tuple.Item1(this, tuple.Item2);
 
-				Thread.Sleep(0);
+								Interlocked.Decrement(ref _processingEvent);
+								if (Interlocked.Read(ref _processingEvent) == 0)
+									Signal();
+							});
+
+					continue;
+				}
+
+				WaitSignal();
 			}
+		}
+
+		void Signal()
+		{
+			lock (this)
+				_event.Set();
+		}
+
+		void ResetSignal()
+		{
+			lock (this)
+				_event.Reset();
+		}
+
+		void WaitSignal(int millisecondsTimeout = -1)
+		{
+			// ReSharper disable once InconsistentlySynchronizedField
+			_event.Wait(millisecondsTimeout);
 		}
 	}
 }
