aa2319859b1a352efd37bc2b825ade34f639d3b3
Marco Baldessari <mb@sinformatic.com>
2018-03-30 20:05:35 +0200

    Remove unused using in PathService test

--- FxCommonStandard/Services/PathService.cs
+++ FxCommonStandard/Services/PathService.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using FxCommonStandard.Contracts;
-using FxCommonStandard.Extensions;
 
 namespace FxCommonStandard.Services
 {
